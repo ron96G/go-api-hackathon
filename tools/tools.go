b872447b6cdd17f24630ba5eebae80a40234eb9b
Ron Gummich <ron.gummich@telekom.de>
2025-05-12 15:13:53 +0200

    added openapi gen config

---
+++ tools/tools.go
@@ -0,0 +1,7 @@
+package tools
+
+import (
+	_ "github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen"
+)
+
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=generate-server1.yaml ../api/ap1i.yaml
